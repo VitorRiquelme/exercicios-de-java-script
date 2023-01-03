3f333207f6445ceb6e73c17f94964e653f4919d9
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-03 18:42:10 -0300

    Update index.js

--- exercicios/ex01/index.js
+++ exercicios/ex01/index.js
@@ -1,1 +1,12 @@
-console.log("hllow world")
+let evennums = [];
+function returnEvenValues(array){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i] % 2 === 0 ){
+            evennums.push(array[i]);
+        }
+    }
+    console.log(evennums)
+}
+let array = [1, 2, 3, 4, 5,6, 7, 8];
+
+returnEvenValues(array)

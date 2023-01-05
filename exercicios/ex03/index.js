6d91bbd7b3eab399f52350198419cd2fa62d4275
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-05 20:39:24 -0300

    .

--- exercicios/ex03/index.js
+++ exercicios/ex03/index.js
@@ -5,4 +5,11 @@ let nome = `Meu nome é ${primeiroNome} e meu sobrenome é ${sobreNome}`
 
 console.log(nome)*/
 
-let nota = prompt("qual a sua nota")
+/*let idade = prompt("Qual a sua idade ? ")
+
+if(idade <=17){
+    alert("voce  não pode beber")
+}else if(idade>=18){
+    alert("voce pode beber ")
+}*/
+

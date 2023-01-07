4ab87688ed2361aeaacf619dea070d3f697cc65e
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-07 02:42:32 -0300

    .

--- exercicios/ex04/index.js
+++ exercicios/ex04/index.js
@@ -1,30 +1,30 @@
-
 function calculadora(){
-    const OPERACAO = prompt('Escolha uma operação :\n 1 - soma (+)\n 2 - subtração (-)\n 3 - multiplicação (*)\n 4 - divisão real (/) \n 5 - divisão inteira (%) \n 6 - pontenciação(**)');
+    const OPERACAO = Number(prompt(`Escolha uma operação:\n 1 - soma (+) \n 2 - subtração(-) \n 3 - multiplicação(*) \n 4 - divisão real (/) \n 5 - divisão inteira (%) \n 6 - potenciação(**) `));
 
-    let n1 = prompt('Insira o valor aqui');
-    let n2 = prompt('Insira o outro valor ');
+    let n1 = Number(prompt('Informe seu primeiro valor seu filho da puta idiota'));
+    let n2 = Number(prompt('Informe o segundo falro seu filho de uma puta burro'));
     let resultado;
 
     function soma(){
-        resultado = n1 + n2
-        alert(`A soma entre ${n1} e ${n2} é igual a ${resultado}`)
+        resultado = n1 + n2;
+        alert(`o valor de ${n1} + ${n2} é igual a ${resultado}`);
     }
 
+    if(OPERACAO == 1){
+        soma();
+    }else if(OPERACAO == 2){
+        subtracao();
+    }else if(OPERACAO == 3){
+        multiplicacao();
+    }else if(OPERACAO == 4){
+        divisaoReal();
+    }else if(OPERACAO == 5){
+        divisaoInteira();
+    }else if (OPERACAO == 6){
+        potenciacao();
+    }
+ 
 }
 
-if(OPERACAO == 1){
-    soma(); 
-}else if(OPERACAO == 2){
-    subtraçao();
-}else if(OPERACAO == 3){
-    multiplicacao();
-}else if(OPERACAO == 4){
-    divisaoReal();
-}else if(OPERACAO == 5){
-    divisaoInteira();
-}else if(OPERACAO == 6){
-    potenciacao();
-}
 
 calculadora();

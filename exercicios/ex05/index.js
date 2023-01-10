567a625c2d60600b31804c6d2235beac0cc80073
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-10 04:30:45 -0300

    Update index.js

--- exercicios/ex05/index.js
+++ exercicios/ex05/index.js
@@ -3,20 +3,3 @@ let omedaia = 10
 let resultado = omedaia + omedaio
 console.log(resultado)*/
 
-
-
-const alunosAprovados = [{
-    Nome:"vitor",
-    Nota: 80,
-
-    Nome:"Lucas",
-    Nota: 20,
-}];
-
-function alunosAprovadoss(array, media){
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        if(media >= alunosAprovados.Nota){
-            return alunosAprovados
-        }
-    }};

c0d888100478b3596602de926d5c5f9c12b1a8ba
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-10 18:33:18 -0300

    Update index.js

--- exercicios/ex05/index.js
+++ exercicios/ex05/index.js
@@ -3,3 +3,10 @@ let omedaia = 10
 let resultado = omedaia + omedaio
 console.log(resultado)*/
 
+const PESSOA = {
+    nome:"vitor",
+    idade: 20 ,
+    peso: 20 + "kg", 
+}
+
+console.log(PESSOA.nome)

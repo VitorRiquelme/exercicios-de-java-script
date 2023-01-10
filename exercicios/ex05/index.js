8b1db859ad1a8ca7fef9161ed59bad249270f5aa
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-10 20:21:49 -0300

    Update index.js

--- exercicios/ex05/index.js
+++ exercicios/ex05/index.js
@@ -11,3 +11,20 @@ const PESSOA = {
 
 console.log(PESSOA.nome)
 
+ Calcule a média de diversas notas digitadas pelo usuário.*/
+
+ const NOTAS = prompt("digite sua nota")
+
+ function calcularNotas(NOTAS){
+    if(NOTAS <= 20){
+        window.alert("Você está muito abaixo da média")
+    }else if(NOTAS <= 59){
+        window.alert("Você está abaixo da média")
+    }else if(NOTAS >=60){
+        window.alert("VOcê está acima da média")
+    }else if(NOTAS >=100){
+        window.alert("você está muito acimda da média")
+    }
+ }
+
+ calcularNotas(NOTAS)

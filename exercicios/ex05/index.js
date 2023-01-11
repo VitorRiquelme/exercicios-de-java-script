e879f58d38ad6615a89e30afb3c60ede4e82781d
Vitor riquelme <106284034+VitorRiquelme@users.noreply.github.com>
2023-01-10 21:22:22 -0300

    Update index.js

--- exercicios/ex05/index.js
+++ exercicios/ex05/index.js
@@ -1,44 +1,5 @@
-/*let omedaio = 12
-let omedaia = 10 
-let resultado = omedaia + omedaio
-console.log(resultado)
-
-const PESSOA = {
-    nome:"vitor",
-    idade: 20 ,
-    peso: 20 + "kg", 
-}
-
-console.log(PESSOA.nome)
-
- Calcule a média de diversas notas digitadas pelo usuário.
-
-const NOTAS = Number(prompt("Digita sua nota"));
-
-if(!NOTAS || NOTAS < 0 ){
-    alert("Ops algo deu errado insira um valor valido")
-}else{
-    function calcularNotas(){
-        if(NOTAS <= 20){
-            alert("Você está muito abaixo da média")
-        }else if(NOTAS <= 59){
-            alert("Você está abaixo da média")
-        }else if(NOTAS == 60){
-            alert("VOcê está na média")
-        }else if(NOTAS >= 61){
-            alert("voce está acima da média")
-        }else if(NOTAS > 100){
-            alert("você está muito acimda da média")
-        }
-     }
-    
-     calcularNotas(NOTAS)
-};
 
 
-
-Peça ao usuário para digitar 5 números em uma caixa de texto. Verifique qual é o maior número e exiba-o.
-*/
 
 
 
